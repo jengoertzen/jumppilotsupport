1eb69e5020bc106981da4b8504a3b52fef0dc2bd
Jen Goertzen <jennifer.goertzen@Jen-MacBook.local>
2018-04-30 17:07:53 -0600

    added apostrophe

---
+++ cache/compiled/files/0024e776700acee12a3323fbf5a07668.yaml.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/jennifer.goertzen/Sites/jumppilotsupport/web/user/plugins/error/error.yaml',
+    'modified' => 1523555836,
+    'data' => [
+        'enabled' => true,
+        'routes' => [
+            404 => '/error'
+        ]
+    ]
+];
---
+++ cache/compiled/files/c837eea61246a7af31d11bc949b27eb5.yaml.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/jennifer.goertzen/Sites/jumppilotsupport/web/user/plugins/problems/problems.yaml',
+    'modified' => 1523555838,
+    'data' => [
+        'enabled' => true,
+        'built_in_css' => true
+    ]
+];
