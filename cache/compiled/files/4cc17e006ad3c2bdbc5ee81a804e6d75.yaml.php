dda6c4c4b3b50b65271e8dc90a7ff11ab4734b38
Dustin Horrock <dustinhorrock@gmail.com>
2018-05-08 12:24:12 -0500

    Added Admin

---
+++ cache/compiled/files/4cc17e006ad3c2bdbc5ee81a804e6d75.yaml.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'themes://learn2/learn2.yaml',
+    'modified' => 1525800054,
+    'data' => [
+        'enabled' => true,
+        'root_page' => NULL,
+        'top_level_version' => false,
+        'show_all_pages' => false,
+        'google_analytics_code' => 'UA-118690013-1',
+        'home_url' => 'http://jumppilotsupport.com/',
+        'github' => [
+            'position' => 'top',
+            'tree' => 'https://github.com/getgrav/grav-skeleton-rtfm-site/blob/develop/',
+            'commits' => 'https://github.com/getgrav/grav-skeleton-rtfm-site/commits/develop/'
+        ]
+    ]
+];
---
+++ cache/compiled/files/9442c3d1a8b8a452cafa8620b864ec29.yaml.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/dustin.horrock/jumppilotsupport/user/plugins/problems/problems.yaml',
+    'modified' => 1525800054,
+    'data' => [
+        'enabled' => true,
+        'built_in_css' => true
+    ]
+];
