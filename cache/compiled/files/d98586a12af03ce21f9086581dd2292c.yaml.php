34ad57d5c7f86f69076c26cbf1adf34a563a7cb6
jengoertzen <jennifer.goertzen@Jen-MacBook.local>
2018-05-08 14:06:39 -0500

    testing push

--- cache/compiled/files/d98586a12af03ce21f9086581dd2292c.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/dustin.horrock/jumppilotsupport/user/config/security.yaml',
-    'modified' => 1525800054,
-    'data' => [
-        'salt' => 'rZq6jJTiRfb29B'
-    ]
-];
